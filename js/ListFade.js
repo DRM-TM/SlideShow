4b183ca1cfa29c20128a7912b619bbf4a4d3eedc
Mendiondo Loris <loris.mendiondo@gmail.com>
2014-09-16 10:17:17 +0200

    initial commit

---
+++ js/ListFade.js
@@ -0,0 +1,12 @@
+﻿
+var items, speed;
+
+items = $('ul li')
+speed = 500;
+$(document).ready(function (e) {
+
+    items.each(function (i, element) {
+        $(element).delay(speed * i).fadeIn(speed);
+    })
+
+})
